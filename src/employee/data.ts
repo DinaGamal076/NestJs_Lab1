bec5199be5154b464e99f143eaf9920da6846fe6
Dina Gamal <dinagamal794@gmail.com>
2025-05-17 17:01:16 +0300

    Update data.ts

--- src/employee/data.ts
+++ src/employee/data.ts
@@ -1,5 +1,7 @@
 export const employees = [
-  { id: 1, name: 'John', age: '30', salary: 4000 },
-  { id: 2, name: 'Sara', age: '28', salary: 5000 },
-  { id: 3, name: 'Mike', age: '35', salary: 6000 },
+  { id: 1, name: 'Dina ', age: '23', salary: 9000 },
+  { id: 2, name: 'Nada', age: '23', salary: 10000 },
+  { id: 3, name: 'Mai', age: '22', salary: 6000 },
+    { id: 3, name: 'Nermeen', age: '21', salary: 7000 },
+
 ];
